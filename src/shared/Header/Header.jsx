90a736e8087aa3daa9ffe47e3a78eb096ee1cd98
Shamil Gasanov <74822512+Monvis@users.noreply.github.com>
2025-09-20 22:10:35 +0300

    feat: add theme change functionality to the page
    * replace css selector values with variables

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { GlobalSvgSelector } from '../../assets/icons/global/globalSvgSelector'
 import Select from 'react-select'
 import s from './Header.module.scss'
@@ -16,12 +16,41 @@ export const Header = () => {
       width: '194px',
       height: '37px',
       color: '#FFF',
-      backgroundColor: '#4F4F4F',
+      backgroundColor: theme === 'light' ? 'rgb(71, 147, 255, 0.2)' : '#4F4F4F',
       border: 'none',
       borderRadius: '10px',
+    }),
 
+    singleValue: styles => ({
+      ...styles,
+      color: theme === 'light' ? '#000' : '#FFF',
     })
   }
+
+  const [theme, setTheme] = useState('light')
+
+  const changeTheme = () => {
+    setTheme(theme === 'light'? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    const root = document.querySelector(':root')
+
+    const components = [
+      'body-background',
+      'components-background',
+      'card-background',
+      'card-box-shadow',
+      'text-color'
+    ]
+
+    components.map((elem) => {
+      root.style.setProperty(
+        `--${elem}-default`,
+        `var(--${elem}-${theme})`
+      )
+    })
+  }, [theme])
 
   return (
     <header className={s.header}>
@@ -34,7 +63,7 @@ export const Header = () => {
         </div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change_theme}>
+        <div onClick={changeTheme} className={s.change_theme}>
           <GlobalSvgSelector id='change-theme' />
         </div>
         <Select className={s.select} options={options} defaultValue={options[0]} styles={colourStyles} />
