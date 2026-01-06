47294d614530b1b6c7c03ea8ebee9eafe229f2a7
Shamil Gasanov <74822512+Monvis@users.noreply.github.com>
2026-01-07 01:30:23 +0300

    clean: remove old JSX files after migration

--- src/pages/MonthStatistics/components/MonthStatistics.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import s from './MonthStatistics.module.scss'
-
-export const MonthStatistics = () => {
-  return (
-    <div className={s.MonthStatisticts}></div>
-  )
-}
